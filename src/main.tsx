bf1673d9e4bd8afd33eff6aa35c12c767d77ed85
Juan Carreño <jcarrenoar@unal.edu.co>
2025-03-10 14:33:51 -0500

    fix apollo innecesary import

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import MainRoutes from './MainRoutes'
-import { ApolloProvider } from '@apollo/client'
-import client from './services/ApolloClient'
 import './style/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-        <MainRoutes/>
-      </BrowserRouter>
-    </ApolloProvider>
+    <BrowserRouter>
+      <MainRoutes/>
+    </BrowserRouter>
   </StrictMode>,
 )
